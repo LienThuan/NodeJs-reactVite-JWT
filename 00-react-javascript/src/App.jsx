636ecbdf3fb6919890a79b412b24e75280139a4c
“LienThuan9697” <lienthuan2004@gmail.com>
2025-03-22 09:44:57 +0700

    step1

---
+++ 00-react-javascript/src/App.jsx
@@ -0,0 +1,26 @@
+import axios from "./util/axios.customize"
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+    const fetchhelloworld = async () => {
+      const res = await axios.get(`/v1/api/`)
+      console.log("respouse", res)
+  }
+  fetchhelloworld()
+}, [])
+
+
+
+
+
+
+
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default App
